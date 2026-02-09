a474b2d31853831a5760ad1133688d89152cdf61
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-02-10 09:44:15 +1300

    Moved constants to their own file

---
+++ constants.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+constants.py
+
+Constants for the app.
+"""
+
+ETHCNICITIES = ["Asian", "European", "Māori", "MELAA/Other", "Pacific Peoples"]
+GCH = ["U1", "U2", "R1", "R2", "R3"]
+CATEGORIES = {
+    None: "Placeholder",
+    "Obstetrics & Gynaecology": [
+        "Cervical & Vulval",
+        "Obstetrics",
+        "Hysterectomy & Uterus",
+        "Ovarian & Adnexal",
+        "Other Gynaecology",
+        "Pelvic Floor & Prolapse",
+    ],
+    "General Surgery": [
+        "Colorectal",
+        "Miscellaneous Abdominal / Peritoneal",
+        "Breast",
+        "Hernia/Abdominal Wall",
+        "HPB",
+        "Endocrine",
+        "Upper GI",
+    ],
+    "Plastics": [
+        "Other Soft Tissue",
+        "Hand Surgery",
+        "Flaps & Reconstructions",
+        "Burns",
+        "Scar Revision & Cosmetic",
+    ],
+    "Orthopaedics": [
+        "Fracture / Trauma Fixation",
+        "Other Orthopaedics",
+        "Joint Replacement",
+        "Spine",
+        "Arthroscopy",
+        "Amputations",
+    ],
+    "Ophthalmology": "Ophthalmology",
+    "ENT": [
+        "Throat / Larynx / Airway",
+        "Nasal / Sinus Surgery",
+        "Ear Surgery",
+        "Head & Neck Tumour / Resection",
+        "Other ENT",
+    ],
+    "Oral & Maxillofacial": "Oral & Maxillofacial",
+    "Neurosurgery": ["Cranial", "Spine", "Peripheral Nerve", "Other Neurosurgery"],
+    "Urology": [
+        "Bladder",
+        "Testis / Scrotum / Penis",
+        "Ureter",
+        "Prostate",
+        "Kidney",
+        "Other Urology",
+    ],
+    "Vascular": [
+        "Peripheral Arterial",
+        "Aortic",
+        "Carotid",
+        "Venous",
+        "Other Vascular",
+        "Mesenteric / Visceral",
+    ],
+    "Cardiac": [
+        "Coronary Artery Bypass (CABG)",
+        "Valve Surgery",
+        "Aortic Root / Ascending Aorta",
+        "Other Cardiac",
+    ],
+    "Thoracic": [
+        "Pleural Procedures",
+        "Lung Resection",
+        "Mediastinal",
+        "Other Thoracic",
+    ],
+    "Transplant": ["Kidney", "Liver", "Heart", "Lung", "Pancreas"],
+    "Other": "Other",
+}
+COLUMNS = [
+    "AGE",
+    "ETHNICITY_ORIGINAL",
+    "SEX_ORIGINAL",
+    "DEP18_ORIGINAL",
+    "GCH2018_ORIGINAL",
+    "M3_SCORE",
+    "PRIOR_CANCER",
+    "FACILITY",
+    "ADMISSION_ACUITY",
+    "ADMISSION_SOURCE",
+    "CATEGORY_LEVEL_1",
+    "CATEGORY_LEVEL_2",
+    "OP_SEVERITY",
+    "TRAUMA",
+]
+LABEL_MAP = {
+    "ALL": "All",
+    "MORTALITY_30D": "30-day mortality",
+    "ANY_COMP": "Any complications",
+    "SSI": "SSI",
+    "VTE": "VTE",
+    "CARDIAC_ARREST": "Cardiac arrest",
+    "SEPSIS": "Sepsis",
+    "RESPIRATORY_FAILURE": "Respiratory failure",
+    "SHOCK": "Shock",
+    "STROKE": "Stroke",
+    "AKI": "AKI",
+    "CARDIAC_ARRHYTHMIA": "Cardiac arrhythmia",
+    "DELIRIUM": "Delirium",
+    "GI_BLEEDING": "GI bleeding",
+    "HAEMORRHAGE": "Haemorrhage",
+    "IMPLANT_GRAFT": "Implant graft",
+    "MYOCARDIAL_INFARCTION": "Myocardial infarction",
+    "PNEUMONIA": "Pneumonia",
+    "UTI": "UTI",
+}
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from numpy import arange, expand_dims, zeros
 from pandas import DataFrame, to_numeric
 from pyrisk.models.core import load_pipeline
 
+from constants import CATEGORIES, COLUMNS, ETHCNICITIES, GCH, LABEL_MAP
+
 
 @st.cache_resource
 def app_load_pipeline():
@@ -22,121 +24,7 @@ def convert_dtypes(data):
     return data
 
 
-ETHCNICITIES = ["Asian", "European", "Māori", "MELAA/Other", "Pacific Peoples"]
-GCH = ["U1", "U2", "R1", "R2", "R3"]
-CATEGORIES = {
-    None: "Placeholder",
-    "Obstetrics & Gynaecology": [
-        "Cervical & Vulval",
-        "Obstetrics",
-        "Hysterectomy & Uterus",
-        "Ovarian & Adnexal",
-        "Other Gynaecology",
-        "Pelvic Floor & Prolapse",
-    ],
-    "General Surgery": [
-        "Colorectal",
-        "Miscellaneous Abdominal / Peritoneal",
-        "Breast",
-        "Hernia/Abdominal Wall",
-        "HPB",
-        "Endocrine",
-        "Upper GI",
-    ],
-    "Plastics": [
-        "Other Soft Tissue",
-        "Hand Surgery",
-        "Flaps & Reconstructions",
-        "Burns",
-        "Scar Revision & Cosmetic",
-    ],
-    "Orthopaedics": [
-        "Fracture / Trauma Fixation",
-        "Other Orthopaedics",
-        "Joint Replacement",
-        "Spine",
-        "Arthroscopy",
-        "Amputations",
-    ],
-    "Ophthalmology": "Ophthalmology",
-    "ENT": [
-        "Throat / Larynx / Airway",
-        "Nasal / Sinus Surgery",
-        "Ear Surgery",
-        "Head & Neck Tumour / Resection",
-        "Other ENT",
-    ],
-    "Oral & Maxillofacial": "Oral & Maxillofacial",
-    "Neurosurgery": ["Cranial", "Spine", "Peripheral Nerve", "Other Neurosurgery"],
-    "Urology": [
-        "Bladder",
-        "Testis / Scrotum / Penis",
-        "Ureter",
-        "Prostate",
-        "Kidney",
-        "Other Urology",
-    ],
-    "Vascular": [
-        "Peripheral Arterial",
-        "Aortic",
-        "Carotid",
-        "Venous",
-        "Other Vascular",
-        "Mesenteric / Visceral",
-    ],
-    "Cardiac": [
-        "Coronary Artery Bypass (CABG)",
-        "Valve Surgery",
-        "Aortic Root / Ascending Aorta",
-        "Other Cardiac",
-    ],
-    "Thoracic": [
-        "Pleural Procedures",
-        "Lung Resection",
-        "Mediastinal",
-        "Other Thoracic",
-    ],
-    "Transplant": ["Kidney", "Liver", "Heart", "Lung", "Pancreas"],
-    "Other": "Other",
-}
-COLUMNS = [
-    "AGE",
-    "ETHNICITY_ORIGINAL",
-    "SEX_ORIGINAL",
-    "DEP18_ORIGINAL",
-    "GCH2018_ORIGINAL",
-    "M3_SCORE",
-    "PRIOR_CANCER",
-    "FACILITY",
-    "ADMISSION_ACUITY",
-    "ADMISSION_SOURCE",
-    "CATEGORY_LEVEL_1",
-    "CATEGORY_LEVEL_2",
-    "OP_SEVERITY",
-    "TRAUMA",
-]
-LABEL_MAP = {
-    "ALL": "All",
-    "MORTALITY_30D": "30-day mortality",
-    "ANY_COMP": "Any complications",
-    "SSI": "SSI",
-    "VTE": "VTE",
-    "CARDIAC_ARREST": "Cardiac arrest",
-    "SEPSIS": "Sepsis",
-    "RESPIRATORY_FAILURE": "Respiratory failure",
-    "SHOCK": "Shock",
-    "STROKE": "Stroke",
-    "AKI": "AKI",
-    "CARDIAC_ARRHYTHMIA": "Cardiac arrhythmia",
-    "DELIRIUM": "Delirium",
-    "GI_BLEEDING": "GI bleeding",
-    "HAEMORRHAGE": "Haemorrhage",
-    "IMPLANT_GRAFT": "Implant graft",
-    "MYOCARDIAL_INFARCTION": "Myocardial infarction",
-    "PNEUMONIA": "Pneumonia",
-    "UTI": "UTI",
-}
-
+# Define some constants
 if "model_run" not in st.session_state:
     # Session state flag to check if model has run at least once
     st.session_state.model_run = False
