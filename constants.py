6025b2adc0b53f4d9d2d22ab41ea9aa709a2e04b
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-04-15 09:12:35 +1200

    Updated constants to include operation averages name

--- constants.py
+++ constants.py
@@ -9,7 +9,8 @@ Constants for the app.
 MODEL_NAME = "ai_risk_HGBc-v0.5.1.0-a.1.3.0.joblib"
 MODEL = "assets/models/" + MODEL_NAME
 BUCKET = "gs://nz-risk-score-bucket"
-AVERAGES = "assets/models/op_averages.joblib"
+AVERAGES_NAME = "op-averages.joblib"
+AVERAGES = "assets/models/" + AVERAGES_NAME
 ETHCNICITIES = ["Asian", "European", "Māori", "MELAA/Other", "Pacific Peoples"]
 CATEGORIES = {
     None: "Placeholder",
