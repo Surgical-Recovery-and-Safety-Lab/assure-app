5174921f1fba401c5b796f951d5b9ebda77b4abd
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-04-21 16:14:22 +1200

    Added a home page

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ if "consent" not in st.session_state:
     # Session state varible for consent
     st.session_state.consent = False
 
+home_page = st.Page("home_page.py", title="Home", icon="🏠")
 about_page = st.Page("about_page.py", title="About")
-calc_page = st.Page("main_page.py", title="Calculator")
-pg = st.navigation([calc_page, about_page])
+calc_page = st.Page("main_page.py", title="Calculator", icon="🎯")
+pg = st.navigation([home_page, about_page, calc_page])
 pg.run()
