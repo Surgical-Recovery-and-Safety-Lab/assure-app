1db12f058da581082774525f3f253f1e18295856
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-04-23 13:26:37 +1200

    Updated to send email via Formspree

--- contact_page.py
+++ contact_page.py
@@ -8,20 +8,25 @@ Streamlit ASSuRE app contact page.
 
 import streamlit as st
 
+from app_fn import send_email
+
 st.header("Contact us", divider="rainbow")
 st.write("""Get in touch with us to report an issue, ask a question, or provide some
     feedback by filling out the form below.
     """)
-with st.form("contact_form"):
+with st.form("contact_form", enter_to_submit=False):
     st.subheader("Contact form")
-    name = st.text_input("Name")
     email = st.text_input("Email address")
+    subject = st.text_input("Subject")
     message = st.text_area("Message")
 
     submitted = st.form_submit_button("Send message")
 
     if submitted:
-        if email and name and message:
-            st.success("Thank you, your message has been sent!")
+        if email and subject and message:
+            with st.spinner("Sending..."):
+                success = send_email(email, subject, message)
+                if success:
+                    st.success("Thank you, your message has been sent!")
         else:
             st.error("Please fill out all fields.")
